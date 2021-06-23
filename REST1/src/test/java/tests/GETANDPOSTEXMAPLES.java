b8ae91dd5033b61e1bab6fcd4472a641995c243d
mahimarajendra <mahimarajendra1@gmail.com>
2021-06-23 21:26:57 +0530

    First Commit

---
+++ REST1/src/test/java/tests/GETANDPOSTEXMAPLES.java
@@ -0,0 +1,46 @@
+package tests;
+
+import org.json.simple.JSONObject;
+import org.testng.annotations.Test;
+
+import io.restassured.http.ContentType;
+
+import static io.restassured.RestAssured.*;
+import static org.hamcrest.Matchers.*;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class GETANDPOSTEXMAPLES {
+
+	//@Test
+	public void Testget() {
+	 
+		baseURI= "https://reqres.in/api";
+		given().get("/users?page=2").then().statusCode(200).
+		body("data[1].first_name",equalTo("Lindsay")).
+		body("data.first_name",hasItems("George","Rachel"));
+	}
+	
+	@Test
+	public void Testpost()
+	{
+		Map<String,Object> map= new HashMap<String, Object>();
+		map.put("name", "Maahi");
+		map.put("job", "engineer");
+		System.out.println(map);
+		JSONObject request = new JSONObject(map);
+		System.out.println(request);
+		
+		baseURI= "https://reqres.in/api";
+		given().
+		header("Content-Type","application/json").
+		contentType(ContentType.JSON).
+		accept(ContentType.JSON).
+			body(request.toJSONString()).
+		when().
+			post("/users").
+		then().
+			statusCode(201).log().all();
+	}
+}
